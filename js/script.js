c20696cd2e2b65287914c2d6ec6dc2ac330c535c
JonatanMendez17 <Jonatanmendez17@hotmail.com>
2025-05-01 01:45:02 -0300

    Se ajusta diseño, se agrega logica

--- js/script.js
+++ js/script.js
@@ -3,4 +3,20 @@ const botones = document.querySelectorAll('[data-valor]');
 const btnIgual = document.getElementById('igual');
 const btnLimpiar = document.getElementById('limpiar');
 
-botones.forEach
+botones.forEach(boton => {
+    boton.addEventListener('click', () => {
+        display.value += boton.getAttribute('data-valor');
+    });
+});
+
+btnIgual.addEventListener('click', () => {
+    try{
+        display.value = eval(display.value);
+    } catch {
+        display.value = 'error';
+    }
+});
+
+btnLimpiar.addEventListener('click', () => {
+    display.value = '';
+})
