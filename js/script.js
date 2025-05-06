2363582e4d22bf38dbc61a5bc753cb8f4cd0a3af
JonatanMendez17 <Jonatanmendez17@hotmail.com>
2025-05-05 23:38:31 -0300

    Mejora de interfaz e implementacion de logica

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
+// Se declaran las constantes para manipular los elementos
 const display = document.getElementById('display');
 const botones = document.querySelectorAll('[data-valor]');
 const btnIgual = document.getElementById('igual');
 const btnLimpiar = document.getElementById('limpiar');
 
+
+// Agrega el valor del boton presionado al contenido actual del display
 botones.forEach(boton => {
     boton.addEventListener('click', () => {
         display.value += boton.getAttribute('data-valor');
     });
 });
 
+// Convierte el valor del display en una operacion aritmetica
 btnIgual.addEventListener('click', () => {
     try{
         display.value = eval(display.value);
@@ -17,6 +21,7 @@ btnIgual.addEventListener('click', () => {
     }
 });
 
+// Limpia el contenido del display
 btnLimpiar.addEventListener('click', () => {
     display.value = '';
 })
